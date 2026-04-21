e5e6c7713e2feade0ca7c9e1301f4a783d81464c
tinafredriksson <tina.mfredriksson@gmail.com>
2026-04-21 10:23:10 +0200

    added functions for start and stop the game

--- js/app.js
+++ js/app.js
@@ -7,16 +7,28 @@
 // Variables
 let score = 0;
 let timeleft = 60;
+let gameStarted = false;
+let gameEnded = false;
+let interval = null;
 
 // HTML DOM
 const button1= document.getElementById("button1");
+const button2= document.getElementById("button2");
 const scoreDisplay= document.getElementById("scoreDisplay");
 const timerDisplay= document.getElementById("timerDisplay");
+const label1 = document.getElementById('label1');
+const input1 = document.getElementById('name');
+
 
 // UI Functions
 button1.addEventListener("click", ()=> {
-increaseScore();
+  if (!gameEnded) {
+    increaseScore();
+  }
+  if (!gameStarted) {
+startGame();
 })
+
 //TODO: start only when "click Me is Clicked"
 setInterval(countdown,  1000);
 
@@ -26,10 +38,23 @@ function increaseScore() {
  scoreDisplay.innerText = score;
 }
 
-function countdown(){
+function countdown() {
   timeleft--;
   timerDisplay.innerText = timeleft;
-// TODO: Stop timer att the end.
+
+  if (timeleft <= 0) {
+    timerDisplay.innerText = 0;
+    endGame();
+  }
 }
 
+  // TODO: Stop timer att the end.
+function  startGame(){
+  setInterval(countdown, 1000);
+  gameStarted = true;
+}
+
+function endGame() {
+  gameEnded = true;
+}
 
